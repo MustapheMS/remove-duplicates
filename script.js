3f84fc9c71bbca9ba1c4ee83f1bb9b53152b1fc0
MustapheMS <moztaphem9@gmail.com>
2023-06-15 23:05:24 +0300

    Duplicate Done 100%

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Good Luck 💪🏾
+
+//Done ✌️
+
+function removeDuplicates(arr){
+    return arr.filter((dupli, index)=>{
+        return (arr.indexOf(dupli) === index);
+    })
+}console.log(removeDuplicates([4,5,6,10,4,5,10,2,9,8,1,8]));
+console.log(removeDuplicates(["Hassan", "Aisha", "Aisha"]));
+console.log(removeDuplicates(["Sa'ad", "Mustaphe", "Mustaphe"]));
